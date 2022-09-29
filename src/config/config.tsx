1abedd67f7f979be26411bdbf9a7526ce8a60d58
Dong-U <sdwsgkj@hanmail.net>
2022-09-29 18:04:03 +0900

    fix : backend server url 획득 코드 수정

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,5 +1,5 @@
 export const getBackendServerUrl = () => {
   const env = process.env.NODE_ENV;
-  const url = env === 'production' ? 'http://yosmart.kr/api' : 'http://localhost:8080';
+  const url = env === 'production' ? '/api' : 'http://localhost:8080';
   return url;
 };
